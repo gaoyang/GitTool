70e1be4bcebcb90ad5f14d74e0d01b2c2838f233
leo <xz@lgysh.cn>
2020-06-01 17:53:40 +0800

    初始化 sln

---
+++ GitTool/Common/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GitTool.Common
+{
+    public class Command : ICommand
+    {
+        readonly Action execute;
+        readonly Func<bool> canExecute;
+
+        public Command(Action action, Func<bool> canExecute = null)
+        {
+            execute = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ GitTool/MainWindow_Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using GitTool.Common;
+using LibGit2Sharp;
+
+namespace GitTool
+{
+    public class MainWindow_Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string RepositoryPath { get; set; }
+
+        public string OldEmail { get; set; }
+
+        public string NewEmail { get; set; }
+
+        public string NewUserName { get; set; }
+
+        public ICommand UpdateCommand => new Command(OnUpdate);
+
+        public void OnUpdate()
+        {
+            var repository = new Repository(@"D:\SourceCode\Me\lgy-studio");
+
+            var head = repository.Head;
+            var trackedBranch = head.TrackedBranch;
+            var identity = new Identity("leo", "xz@lgysh.cn");
+            
+
+            // var rebaseResult = repository.Rebase.Start(head, trackedBranch, null, identity, new RebaseOptions());
+
+
+            Debug.WriteLine("=======Commits====");
+
+            foreach (var commit in repository.Commits)
+            {
+                Debug.WriteLine(commit.Sha);
+                if (commit.Sha == "a8ce505d2abfb7157ce4a98f8b067b4c82f85b69")
+                {
+                    var cherryPickResult = repository.CherryPick(commit, new Signature(identity, DateTimeOffset.Now));
+                    
+                }
+            }
+        }
+    }
+}
