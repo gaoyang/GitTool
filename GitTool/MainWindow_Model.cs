fc40c40bd5785712f77650f4e169fec655636b22
leo <xz@lgysh.cn>
2020-06-05 13:45:49 +0800

    add Gen RSA Key

--- GitTool/MainWindow_Model.cs
+++ GitTool/MainWindow_Model.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Input;
 using System.Windows.Media;
 using GitTool.CLI;
 using GitTool.Common;
 using WPFFolderBrowser;
+using XC.RSAUtil;
 
 namespace GitTool
 {
     public class MainWindow_Model : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public string RepositoryPath { get; set; } = @"D:\SourceCode\Me\lgy-studio";
         public string OldEmail { get; set; }
         public string NewName { get; set; }
@@ -20,8 +21,10 @@ namespace GitTool
         public KeyValuePair<Brush, string> Message { get; set; }
 
         public ICommand SelectPathCommand => new Command(OnSelectPath);
-
         public ICommand UpdateCommand => new Command(OnUpdate);
+        public ICommand FixedMergeCommand => new Command(OnFixedMerge);
+        public ICommand GenRsaKeyCommand => new Command(OnGenRsaKey);
+        public event PropertyChangedEventHandler PropertyChanged;
 
         void OnSelectPath()
         {
@@ -47,6 +50,41 @@ namespace GitTool
             {
                 Message = new KeyValuePair<Brush, string>(Brushes.IndianRed, e.Message);
             }
+        }
+
+        void OnFixedMerge()
+        {
+            Message = default;
+            try
+            {
+                var res = Program.Run(new Params
+                {
+                    RepositoryPath = RepositoryPath,
+                    Email = OldEmail,
+                    NewAuthor = $"{NewName} <{NewEmail}>"
+                });
+                Message = new KeyValuePair<Brush, string>(res.success ? Brushes.ForestGreen : Brushes.IndianRed, res.message);
+            }
+            catch (Exception e)
+            {
+                Message = new KeyValuePair<Brush, string>(Brushes.IndianRed, e.Message);
+            }
+        }
+
+        void OnGenRsaKey()
+        {
+            var dialog = new WPFFolderBrowserDialog();
+            dialog.ShowDialog();
+
+            var keyDir = dialog.FileName;
+            var xmlKey = RsaKeyGenerator.XmlKey(2048);
+
+            File.WriteAllText(Path.Combine(keyDir, $"{DateTime.Now:yyyyMMddHHmmssfff}.private"), xmlKey[0]);
+            File.WriteAllText(Path.Combine(keyDir, $"{DateTime.Now:yyyyMMddHHmmssfff}.public"), xmlKey[1]);
+
+            Process.Start("explorer.exe", keyDir);
+            // new RsaXmlUtil().PrivateRsa.
+            
         }
     }
 }
